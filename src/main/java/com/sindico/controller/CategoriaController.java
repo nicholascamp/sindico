5c4a690dd14ce94baf505fdf16f7f943f3d85e17
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2012-05-30 22:43:05 -0300

    Categoria Controller Layout

--- src/main/java/com/sindico/controller/CategoriaController.java
+++ src/main/java/com/sindico/controller/CategoriaController.java
@@ -1,6 +1,9 @@
 package com.sindico.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.sindico.dao.CategoriaDAO;
 import com.sindico.entity.Categoria;
+import com.sindico.service.CategoriaService;
 
 @Controller
 @SessionAttributes
@@ -19,41 +23,62 @@ public class CategoriaController {
 	@Autowired
 	private CategoriaDAO categoriaDAO;
 	
+	@Autowired
+	private CategoriaService categoriaService;
+	
 	@RequestMapping(method = RequestMethod.GET, value = "/categoria/lista.html")
 	public ModelAndView indexCategoria(){
-		return new ModelAndView();
+		ModelAndView modelAndView = new ModelAndView("/categorias", "categoria", new Categoria());
+		PagedListHolder<Categoria> pagedListHolder = new PagedListHolder<Categoria>(categoriaService.listarCategorias());
+		pagedListHolder.setPageSize(2);
+
+		List<Categoria> pagedListCategorias = pagedListHolder.getPageList();
+
+		modelAndView.addObject("categoria", pagedListCategorias);
+
+		return modelAndView;
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/categoria/mostra.html")
 	public ModelAndView showCategoria(long id){
-		return new ModelAndView();
+		ModelAndView modelAndView = new ModelAndView("/categoria", "categoria", new Categoria());
+		
+		modelAndView.addObject("categoria", categoriaDAO.getCategoria(id));
+		return modelAndView;
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/categoria/cria.html")
 	public ModelAndView newCategoria(){
 		ModelAndView modelAndView = new ModelAndView("/criaCategoria", "categoria", new Categoria());
-		
+			
 		return modelAndView;		
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/categoria/cria.html")
 	public ModelAndView createCategoria(@ModelAttribute("categoria") Categoria categoria, BindingResult result){
-		return new ModelAndView();
+		categoriaService.criarCategoria(categoria);
 		
+		return new ModelAndView("/categoria", "categoria", categoria);
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/categoria/edita.html")
-	public ModelAndView editCategoria(){
-		return new ModelAndView();
+	public ModelAndView editCategoria(long id){
+		Categoria categoria = categoriaDAO.getCategoria(id);
+		
+		return new ModelAndView("/editaCategoria", "categoria", categoria);
 	}
 	
 	@RequestMapping(method = RequestMethod.PUT, value = "/categoria/edita.html")
 	public ModelAndView updateCategoria(Categoria categoria){
-		return new ModelAndView();
+		categoriaDAO.atualizaCategoria(categoria);
+		
+		return new ModelAndView("/categoria", "categoria", categoria);
 	}
 	
 	@RequestMapping(method = RequestMethod.DELETE, value = "/categoria/deleta.html")
 	public ModelAndView destroyCategoria(Categoria categoria){
-		return new ModelAndView();
+		categoriaDAO.removeCategoria((long) categoria.getCodigo());
+		
+		return new ModelAndView("/categorias", "categorias", categoriaService.listarCategorias());
 	}
 }
