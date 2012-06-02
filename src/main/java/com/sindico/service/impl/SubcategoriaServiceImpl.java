2c0287991e3dce801952d905e7972c98b5d550b3
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2012-06-02 14:36:14 -0300

    Subcategoria COntroller

--- src/main/java/com/sindico/service/impl/SubcategoriaServiceImpl.java
+++ src/main/java/com/sindico/service/impl/SubcategoriaServiceImpl.java
@@ -3,11 +3,13 @@ package com.sindico.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.sindico.dao.SubcategoriaDAO;
 import com.sindico.entity.Subcategoria;
 import com.sindico.service.SubcategoriaService;
 
+@Service("subcategoriaService")
 public class SubcategoriaServiceImpl implements SubcategoriaService{
 
 	@Autowired
