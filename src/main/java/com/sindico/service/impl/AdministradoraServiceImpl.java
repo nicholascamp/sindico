99a538beb194d687957e206bc0cd7b47d29146f4
Lucas Oliveira <lucas.loliveira@hotmail.com>
2012-06-10 14:56:03 -0300

    correcao em admService

--- src/main/java/com/sindico/service/impl/AdministradoraServiceImpl.java
+++ src/main/java/com/sindico/service/impl/AdministradoraServiceImpl.java
@@ -3,38 +3,42 @@ package com.sindico.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.sindico.dao.AdministradoraDAO;
 import com.sindico.entity.Administradora;
 import com.sindico.service.AdministradoraService;
 
-public class AdministradoraServiceImpl implements AdministradoraService{
+@Service("administradoraService")
+public class AdministradoraServiceImpl implements AdministradoraService {
 
 	@Autowired
-	AdministradoraDAO dao;
-	
+	AdministradoraDAO	dao;
+
 	@Override
 	public List<Administradora> listAdministradoras() {
-		return dao.getLista(); 
+		return dao.getLista();
 	}
 
 	@Override
-	public Administradora getAdministradora(Long id) {
-		return dao.getAdministradora(id) ;
+	public Administradora getAdministradora(final Long id) {
+		return dao.getAdministradora(id);
 	}
 
 	@Override
-	public Administradora createAdministradora(Administradora administradora) {
+	public Administradora createAdministradora(
+			final Administradora administradora) {
 		return dao.criaAdministradora(administradora);
 	}
 
 	@Override
-	public Administradora updateAdministradora(Administradora administradora) {
+	public Administradora updateAdministradora(
+			final Administradora administradora) {
 		return dao.atualizaAdministradota(administradora);
 	}
 
 	@Override
-	public boolean removeAdministradora(Long id) {
+	public boolean removeAdministradora(final Long id) {
 		return dao.removeAdministradora(id);
 	}
 
