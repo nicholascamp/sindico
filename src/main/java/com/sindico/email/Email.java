4395b6f6861c1122d30362db9cd234922c919935
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2012-07-05 20:45:00 -0300

    Email

---
+++ src/main/java/com/sindico/email/Email.java
@@ -0,0 +1,12 @@
+package com.sindico.email;
+
+import java.util.Properties;
+
+public class Email {
+	public void sendEmail(String email, String nome){
+		Properties props = new Properties();
+		props.put("mail.host","smtp.host.com.br");  
+		
+	}
+
+}
